f4b4c260ed60e620f57be1fce87aff81cdee04bb
marshvin <vincentmarsh01@gmail.com>
2024-02-21 17:43:46 +0300

    Frontend Setup

---
+++ FRONTEND/eventify/src/App.jsx
@@ -0,0 +1,16 @@
+
+import React from 'react';
+import Home from './components/Home';
+import './App.css';
+
+function App() {
+  return (
+    <React.Fragment>
+    <div>
+      <Home />    
+    </div>
+  </React.Fragment>
+  );
+}
+
+export default App;
---
+++ FRONTEND/eventify/src/components/Home.jsx
@@ -0,0 +1,10 @@
+const HomePage = () => {
+    return (
+      <div>
+        <p className="font-bold flex justify-center">HomePage</p>
+      </div>
+    );
+  };
+  
+  export default HomePage;
+  
