560d5f630fe7ce7ca75d5d48b6c7d3fe512e1eae
AbogeJr <junioraboge@gmail.com>
2024-03-09 13:01:07 +0300

    fix: client directory strucutre

---
+++ client/src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+const firebaseConfig = {
+  apiKey: "AIzaSyACL2ash1FDWSUrXIC4Zy8oCG1DBuX9L_Q",
+  authDomain: "real-es-7557d.firebaseapp.com",
+  projectId: "real-es-7557d",
+  storageBucket: "real-es-7557d.appspot.com",
+  messagingSenderId: "984367538212",
+  appId: "1:984367538212:web:cbd9c385c2b6e1713bb8b1",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
